42888ad7db762771a995145b6a70fdd84bb2dcbb
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2016-12-12 16:10:03 -0600

    Create multi-image.py
    will allow you to classify multiple images with TF

---
+++ multi-image.py
@@ -0,0 +1,12 @@
+def main(_):
+  maybe_download_and_extract()
+
+  # search for files in 'images' dir
+  files_dir = os.getcwd() + '/images'
+  files = os.listdir(files_dir)
+
+  # loop over files, print prediction if it is an image
+  for f in files:
+    if f.lower().endswith(('.png', '.jpg', '.jpeg')):
+      image_path = files_dir + '/' + f
+      print run_inference_on_image(image_path)
