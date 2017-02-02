164e050ffc1dbc9212fa678115474366458ab1e6
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2017-02-02 16:18:51 -0600

    Create preDICT.py

---
+++ preDICT.py
@@ -0,0 +1,45 @@
+#import modules
+import sys
+import tensorflow as tf
+from PIL import Image,ImageFilter
+
+def predictint(imvalue):
+    """
+    This function returns the predicted integer.
+    The imput is the pixel values from the imageprepare() function.
+    """
+    
+    # Define the model (same as when creating the model file)
+    x = tf.placeholder(tf.float32, [None, 784])
+    W = tf.Variable(tf.zeros([784, 10]))
+    b = tf.Variable(tf.zeros([10]))
+    y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+    init_op = tf.initialize_all_variables()
+    saver = tf.train.Saver()
+    
+    """
+    Load the model.ckpt file
+    file is stored in the same directory as this python script is started
+    Use the model to predict the integer. Integer is returend as list.
+    Based on the documentatoin at
+    https://www.tensorflow.org/versions/master/how_tos/variables/index.html
+    """
+    with tf.Session() as sess:
+        sess.run(init_op)
+        saver.restore(sess, "model.ckpt")
+        #print ("Model restored.")
+   
+        prediction=tf.argmax(y,1)
+        return prediction.eval(feed_dict={x: [imvalue]}, session=sess)
+
+
+def imageprepare(argv):
+    """
+    This function returns the pixel values.
+    The imput is a png file location.
+    """
+    im = Image.open(argv).convert('L')
+    width = float(im.size[0])
+    height = float(im.size[1])
+    newImage = Image.new('L', (28, 28), (255)) #creates white canvas of 28x28 pixels
