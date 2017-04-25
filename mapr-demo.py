a9ad232de6d017387821b08bfe036e0a11af28e2
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2017-04-25 09:24:57 -0500

    Create mapr-demo.py

---
+++ mapr-demo.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import numpy as np
+
+# Similarly to the example in: https://www.tensorflow.org/tutorials/tflearn/
+# we create a model and test on our own TSA Baggage Claims data.
+
+# separated train and test files from MapR-FS
+TRAIN = "/mapr/demo.mapr.com/user/mapr/claims_train.csv"
+TEST = "/mapr/demo.mapr.com/user/mapr/claims_test.csv"
+MODEL_DIR = "/mapr/demo.mapr.com/user/mapr/model"
+
+# load the data sets
+training_set = tf.contrib.learn.datasets.base.load_csv_with_header(
+    filename=TRAIN,
+    target_dtype=np.int,
+    features_dtype=np.float32)
+test_set = tf.contrib.learn.datasets.base.load_csv_with_header(
+    filename=TEST,
+    target_dtype=np.int,
+    features_dtype=np.float32)
