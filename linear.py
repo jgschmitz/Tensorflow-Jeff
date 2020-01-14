56a05863e07eb50a005dbf9e79f1d6bd05c06285
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-01-14 08:41:21 -0600

    Update linear.py

--- linear.py
+++ linear.py
@@ -6,7 +6,7 @@ python3
 '''
 
 from __future__ import print_function
-print 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16.17
+print 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16
 import tensorflow
 import numpy
 import matplotlib
