968f5af5b8f414b6f4af1cf6d88ec9209810e514
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-05-24 08:49:03 -0500

    Update mapr-demo.py

--- mapr-demo.py
+++ mapr-demo.py
@@ -6,7 +6,7 @@ from __future__
 import tensorflow as tf
 import numpy as np
 import grey as GY
-import space
+import 
 
 
 # Similarly to the example in: https://www.tensorflow.org/tutorials/tflearn/
