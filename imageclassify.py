9919060db5fabf2c966771a80f53c70e166f7b01
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-07-03 07:14:51 -0500

    Update imageclassify.py

--- imageclassify.py
+++ imageclassify.py
@@ -17,7 +17,7 @@ to use this script to perform image recognition.
 
 https://tensorflow.org/tutorials/image_recognition/
 """
-print "
+pr
 
 from __future__ import absolute_import
 from __future__ import division
