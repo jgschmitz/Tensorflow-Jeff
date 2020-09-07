3c0bd5932499944d6ad26fbdcca946f8126f61b9
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-09-07 14:20:23 -0500

    Update DataPoints.py

--- DataPoints.py
+++ DataPoints.py
@@ -2,6 +2,9 @@ python3
 
 import tensorflow as tf
 import numpy as np
+import sklearn as SciKitLearn
+import ggazzy as gzy
+
 
 # Create 100 x, y data points in NumPy, y = x * 0.1 + 0.3
 x_data = np.random.rand(100).astype(np.float32)
