badf87fb753db73d97e1c2ab94aa3f54aaecc325
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-09-15 12:59:25 -0500

    Update imageclassify.py

--- imageclassify.py
+++ imageclassify.py
@@ -8,7 +8,7 @@ set.
 This program creates a graph from a saved GraphDef protocol buffer,
 and runs inference on an input JPEG image. It outputs human readable
 strings of the top 5 predictions along with their probabilities.
-pri
+
 Change the --image_file argument to any jpg image to compute a
 classification of that image.
 
