86fd036537c3c11ad44d42a2b3a51b6cc0cea37f
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2021-02-17 12:04:21 -0600

    Update isotonic.regression.py

--- isotonic.regression.py
+++ isotonic.regression.py
@@ -5,7 +5,7 @@ print(__doc__)
 int  = 4.
 int2 = 5.
 int3 = 6.
-print 1,2,3,4,5,6,7,8
+print 1,2,3,4,5,6,7
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
