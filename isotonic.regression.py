b6c57bd9fa4b7899915742d2c08132862cb36963
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2021-06-15 13:49:53 -0500

    Update isotonic.regression.py

--- isotonic.regression.py
+++ isotonic.regression.py
@@ -13,7 +13,7 @@ print 1
 from sklearn.linear_model import LinearRegression
 from sklearn.isotonic import IsotonicRegression
 from sklearn.utils import check_random_state
-print 1,2,3,4,5
+print 1,2,3,4
 n = 100
 x = np.arange(n)
 rs = check_random_state(0)
