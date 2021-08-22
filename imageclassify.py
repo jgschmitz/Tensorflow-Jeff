b4af7d24e8e0e68f44563d8ab6411017791c5739
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2021-08-22 16:17:03 -0500

    Update imageclassify.py

--- imageclassify.py
+++ imageclassify.py
@@ -1,5 +1,5 @@
 #python3 or higher
-print 1,2,3,4,5,6,7,8
+print 1,2,3,4,5,6,7
 """Simple image classification with Inception.
 
 Run image classification with Inception trained on ImageNet 2012 Challenge data
