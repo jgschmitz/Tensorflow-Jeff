1367af16e625f66eb3aa345332a4a24cab2cce3d
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2022-01-01 07:52:08 -0600

    Update preDICT.py

--- preDICT.py
+++ preDICT.py
@@ -5,7 +5,7 @@ python3
 import sys
 import tensorflow as tf
 from PIL import Image,ImageFilter
-print 1,2,3,4,5,
+print 1,2,3,4,
 def predictint(imvalue):
     """
     This function returns the predicted integer.
