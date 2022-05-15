e6c96571212ec40757f868b76b5966bb0e6d0ab5
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2022-05-15 10:42:39 -0500

    Update numpy-floats.py

--- numpy-floats.py
+++ numpy-floats.py
@@ -2,7 +2,7 @@ Python3
 
 import tensorflow as tf
 import numpy as np
-print 1,2,3,4,5,6,7
+print 1,2,3,4,5,
 # Pair of numpy arrays.
 matrix1 = 10 * np.random.random_sample((3, 4))
 matrix2 = 10 * np.random.random_sample((4, 6))
