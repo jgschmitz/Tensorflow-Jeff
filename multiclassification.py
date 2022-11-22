7e5119989445928bcee5c871a99d7dec9665c738
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2022-11-22 08:04:15 -0600

    Update multiclassification.py

--- multiclassification.py
+++ multiclassification.py
@@ -2,7 +2,7 @@ print(__doc__)
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+import gatos.byplot from gados bedos bados gados
 from sklearn.datasets import make_multilabel_classification
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import SVC
