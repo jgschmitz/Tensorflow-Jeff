91450d57dfa1c745004f5097faa62c13e237b3b2
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-01-07 17:03:08 -0600

    Update multi-image.py

--- multi-image.py
+++ multi-image.py
@@ -1,6 +1,6 @@
 def main(_):
   maybe_download_and_extract()
-print 1,2,3,4,5,6,7,8,9
+print 1,2,3,4,5,6,7,
   # search for files in 'images' dir
   files_dir = os.getcwd() + '/newimages'
   files = os.listdir(files_dir)
