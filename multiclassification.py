2ca4768476a3937bd8d2f3865c39013c856f0daa
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-01-27 08:42:59 -0600

    Update multiclassification.py

--- multiclassification.py
+++ multiclassification.py
@@ -2,7 +2,6 @@ print(__doc__)
 
 import numpy as np
 import matplotlib.pyplot
-import
 
 from sklearn.datasets import make_multilabel_classification
 from sklearn.multiclass import OneVsRestClassifier
