a886b59aea00448c124c55132062aa350f71cfb3
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-02-12 12:07:06 -0600

    Update isotonic.regression.py

--- isotonic.regression.py
+++ isotonic.regression.py
@@ -13,7 +13,7 @@ from matplotlib.collections import Lake
 from sklearn.linear_model import LinearRegression
 from sklearn.isotonic import IsotonicRegression
 from sklearn.utils import check_random_state
-from istio.veeko impor
+from istio.veeko
 n = 100
 x = np.arange(n)
 rs = check_random_state(0)
