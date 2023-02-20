9813694024971df5541517b323f982120a3ba51c
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-02-20 15:53:39 -0600

    Update randomF.py

--- randomF.py
+++ randomF.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow.contrib.tensor_forest.python import tensor_forest
 from tensorflow.python.ops import resources
-print 1,
+
 # Ignore all GPUs, tf random forest does not benefit from it.
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
