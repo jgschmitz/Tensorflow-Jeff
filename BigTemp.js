ad05cc18f710f9a2c7a8e88a5b72412693a03eef
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-05-14 09:15:27 -0500

    Create BigTemp.js

---
+++ BigTemp.js
@@ -0,0 +1,105 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_kinesis_stream" "temp_stream" {
+  name             = "temp-stream"
+  shard_count      = 1
+  retention_period = 24
+}
+
+resource "aws_lambda_function" "temp_lambda" {
+  function_name    = "temp-lambda"
+  role             = aws_iam_role.temp_lambda.arn
+  handler          = "index.handler"
+  runtime          = "nodejs14.x"
+  filename         = "lambda.zip"
+  source_code_hash = filebase64sha256("lambda.zip")
+}
+
+resource "aws_iam_role" "temp_lambda" {
+  name = "temp-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "temp_lambda" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.temp_lambda.name
+}
+
+resource "aws_lambda_permission" "temp_lambda_kinesis" {
+  statement_id  = "AllowExecutionFromKinesis"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.temp_lambda.function_name
+  principal     = "kinesis.amazonaws.com"
+  source_arn    = aws_kinesis_stream.temp_stream.arn
+}
+
+resource "aws_kinesis_firehose_delivery_stream" "temp_delivery_stream" {
+  name        = "temp-delivery-stream"
+  destination = "s3"
+
+  s3_configuration {
+    bucket_arn = aws_s3_bucket.temp_bucket.arn
+    role_arn   = aws_iam_role.temp_delivery_stream.arn
+  }
+}
+
+resource "aws_s3_bucket" "temp_bucket" {
+  bucket = "temp-bucket"
+}
+
+resource "aws_iam_role" "temp_delivery_stream" {
+  name = "temp-delivery-stream-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = {
+          Service = "firehose.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "temp_delivery_stream" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonKinesisFirehoseServiceRolePolicy"
+  role       = aws_iam_role.temp_delivery_stream.name
+}
+
+resource "aws_kinesis_firehose_delivery_stream" "temp_delivery_stream" {
+  name        = "temp-delivery-stream"
+  destination = "lambda"
+
+  lambda_configuration {
+    resource_arn = aws_lambda_function.temp_lambda.arn
+    role_arn     = aws_iam_role.temp_delivery_stream.arn
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "temp_delta_alarm" {
+  alarm_name          = "temp-delta-alarm"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "TemperatureDelta"
+  namespace           = "AWS/Firehose"
+  period              = 60
+  statistic           = "Maximum"
+  threshold           = 5
+ 
