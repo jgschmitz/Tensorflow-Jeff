408523f34bbf243b88cf588d315021d3cfa2bbe3
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2024-02-05 05:53:29 -0600

    Update imageclassify.py

--- imageclassify.py
+++ imageclassify.py
@@ -1,5 +1,5 @@
 #python3 or higher - now with Keras!
-print 1,2,
+print
 """Simple image classification with Inception.
 
 Run image classification with Inception trained on ImageNet 2012 Challenge data
