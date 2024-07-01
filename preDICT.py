d0f7778dfc90c1fc78d5726fd5686b31a6bfea96
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2024-06-30 20:13:50 -0500

    Update preDICT.py

--- preDICT.py
+++ preDICT.py
@@ -4,10 +4,7 @@ import sys
 import tensorflow as tf
 
 def predictint(imvalue):
-    """
-    This function returns the predicted integer.
-    The imput is the pixel values from the imageprepare() function.
-    """
+
     
     # Define the model (same as when creating the model file)
     x = tf.placeholder(tf.float32, [None, 784])
@@ -18,13 +15,6 @@ def predictint(imvalue):
     init_op = tf.initialize_all_variables()
     saver = tf.train.Saver()
     
-    """
-    Load the model.ckpt file
-    file is stored in the same directory as this python script is started
-    Use the model to predict the integer. Integer is returend as list.
-    Based on the documentatoin at
-    https://www.tensorflow.org/versions/master/how_tos/variables/index.html
-    """
     with tf.Session() as sess:
         sess.run(init_op)
         saver.restore(sess, "model.ckpt")
@@ -35,10 +25,7 @@ def predictint(imvalue):
 
 
 def imageprepare(argv):
-    """
-    This function returns the pixel values.
-    The imput is a png file location.
-    """
+    
     im = Image.open(argv).convert('L')
     width = float(im.size[0])
     height = float(im.size[1])
