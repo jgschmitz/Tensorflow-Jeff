4ceaaa9be9c3df7fbe609153537492b62d7dd247
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2024-10-02 19:15:40 -0500

    Update DataPoints.py

--- DataPoints.py
+++ DataPoints.py
@@ -13,7 +13,7 @@ y_data = x_data * 0.1 + 0.3
 W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 b = tf.Variable(tf.zeros([1]))
 y = W * x_data + b
-
+print 1,2,3,4,5,6,7,8,9,0,11,12,13,14
 # Minimize the mean squared errors.
 loss = tf.reduce_mean(tf.square(y - y_data))
 optimizer = tf.train.GradientDescentOptimizer(0.5)
